0762d2d9906a114af0caf9190d401aafd3e9e82b
Pravin Rathod <pravinprathod@outlook.com>
2021-05-14 18:36:53 +0530

    adding functionality to shop app and blog apps also integrate models

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+
+def index(request):
+    return render(request,'shop/index.html')
+
+def about(request):
+    return render(request,'shop/about.html')
+
+def ProdView(request):
+    return HttpResponse('<h1> this is shop product view page...</h1>')
+
+def tracker(request):
+    return HttpResponse('<h1> this is shop tracker page...</h1>')
+
+def contact(request):
+    return HttpResponse('<h1>this is shop contact us page... </h1>')
+
+def checkout(request):
+    return HttpResponse('<h1> this is shop checkout page...</h1>')
+
+# for code deploy on github us ecomorigin instead of origin
+
